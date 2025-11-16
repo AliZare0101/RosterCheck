d35c8aaaa309b831698d4643c0803de49c2e1e06
Mohammad Ali Zare <125291159+AliZare0101@users.noreply.github.com>
2025-11-17 00:32:18 +0330

    Add files via upload

---
+++ RosterCheck.py
@@ -0,0 +1,13 @@
+#tamrin
+
+student = ["ali", "sara", "mahdi", "zahra", "omid"]
+
+user = str(input('Enter your name? '))
+
+
+if user in  student:
+    print(f'Yes, {user} this name is on our list.')
+else:
+    print(f'No, {user} this name is not on our list.')
+
+
